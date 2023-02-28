97f0c9bf3d53888f4c66dea8dbcbb07a2f59fdc9
Kenta Edmonda <91776795+KentaEDM@users.noreply.github.com>
2023-02-28 17:37:13 +0700

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,13 @@
+// constructor: Untuk mendeclare pertama kali apa yang ada dalam sebuah class JS.
+// Con: default nilai, default atribut
+
+class player {
+    constructor (username) {
+    this.username = username
+    }
+}
+
+register() ;{
+    console.log('makasi' + this.username)
+}
+
